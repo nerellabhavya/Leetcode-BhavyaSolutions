f45c33cca915bdc593e3337d503d8937a633ed67
nerellabhavya <nerellabhavyasri@gmail.com>
2025-10-14 22:58:36 +0530

    Time: 3 ms (60.71%), Space: 44.2 MB (78.39%) - LeetHub

---
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int sum=0;
+        for(int i=0;i<seats.length;i++){
+            int move=Math.abs(students[i]-seats[i]);
+            sum+=move;
+        }
+        return sum;
+    }
+}
