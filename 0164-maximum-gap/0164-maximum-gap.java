c496e6f6031b2a178b7fe6a5fb7c5ffc8845490c
nerellabhavya <nerellabhavyasri@gmail.com>
2025-10-18 19:04:18 +0530

    Time: 12 ms (87.35%), Space: 62.2 MB (7.55%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.java
+++ 0164-maximum-gap/0164-maximum-gap.java
@@ -4,19 +4,39 @@ class Solution {
         int ans=0;
         int res=0;
         if(n==0||n==1)return 0;
-        else {
-            Arrays.sort(nums);
-            for(int i=0;i<n-1;i++){
-                res=Math.max(res,Math.abs(nums[i]-nums[i+1]));
-                if(res<1){
-                ans= 0;
-                }
-                else{
-                    ans=res;
-                }
-               
-            }
+        int min=nums[0];
+        int max=nums[0];
+        for(int i=0;i<n;i++){
+            max=Math.max(max,nums[i]);
+            min=Math.min(min,nums[i]);
         }
-        return ans;
+        if(min==max)return 0;
+        int bucketsize=Math.max(1,(max-min)/n-1);
+        int bucketcount=(max-min)/bucketsize+1;
+        int bucketmin[]=new int[bucketcount];
+        int bucketmax[]=new int[bucketcount];
+        boolean used[]=new boolean [bucketcount];
+        for(int i=0;i<bucketcount;i++){
+            bucketmin[i]=Integer.MAX_VALUE;
+            bucketmax[i]=Integer.MIN_VALUE;
+            used[i]=false;
+        }
+        for(int i=0;i<n;i++){
+            int num=nums[i];
+            int idx=(num-min)/bucketsize;
+            used[idx]=true;
+            if(num<bucketmin[idx])bucketmin[idx]=num;
+            if(num>bucketmax[idx])bucketmax[idx]=num;
+        }
+          int prevMax = min;
+        int maxGap = 0;
+        for (int i = 0; i < bucketcount; i++) {
+            if (!used[i]) continue;
+            int gap = bucketmin[i] - prevMax;
+            if (gap > maxGap) maxGap = gap;
+            prevMax = bucketmax[i];
+        }
+
+        return maxGap;
     }
 }
