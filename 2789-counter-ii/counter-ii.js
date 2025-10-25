3292f3ed7f1c72718d216f0111d367091e361238
nerellabhavya <nerellabhavyasri@gmail.com>
2025-10-25 11:19:07 +0530

    Time: 57 ms (28.69%) | Memory: 56.4 MB (64.37%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -5,15 +5,15 @@
 var createCounter = function(init) {
     let  current=init;
     return{
-    increment:function(){
+    increment:()=>{
         current=current+1;
         return current;
     },
-    reset:function(){
+    reset:()=>{
         current=init;
         return current;
     },
-    decrement:function(){
+    decrement:()=>{
         current=current-1;
         return current;
     }
