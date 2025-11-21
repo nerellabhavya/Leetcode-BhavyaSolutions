a5599fd3553a2a63ae71a4da1191b603358d15cf
nerellabhavya <nerellabhavyasri@gmail.com>
2025-11-21 10:10:13 +0530

    Time: 4 ms (97.7%), Space: 45.1 MB (38.11%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,27 +1,33 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer>st=new Stack<>();
-        int n=tokens.length;
-        int res=0;
-        for(int i=0;i<n;i++){
-            String t=tokens[i];
-            if(n==1){
-                res=st.push(Integer.parseInt(t));
+        int n = tokens.length;
+
+        
+        int[] st = new int[n];
+        int top = -1;
+
+        for (int i = 0; i < n; i++) {
+            String t = tokens[i];
+
+            
+            if (t.equals("+") || t.equals("-") || t.equals("*") || t.equals("/")) {
+                int b = st[top--];   
+                int a = st[top--];   
+                int res = 0;
+
+                if (t.equals("+")) res = a + b;
+                else if (t.equals("-")) res = a - b;
+                else if (t.equals("*")) res = a * b;
+                else res = a / b;
+
+                st[++top] = res;  
             }
-            if(t.equals("+")||t.equals("-")||t.equals("*")||t.equals("/")){
-                int b=st.pop();
-                int a=st.pop();
+            else {
                 
-                if(t.equals("+"))res=a+b;
-                else if(t.equals("-"))res=a-b;
-                else if(t.equals("*"))res=a*b;
-                else res=a/b;
-                st.push(res);
-            }
-            else{
-                st.push(Integer.parseInt(t));
+                st[++top] = Integer.parseInt(t);
             }
         }
-        return res;
+
+        return st[top];  
     }
 }
