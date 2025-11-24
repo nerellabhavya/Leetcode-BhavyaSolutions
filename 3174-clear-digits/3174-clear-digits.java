0f8afd1a2d6067bab790a5dcbe9cf31601862074
nerellabhavya <nerellabhavyasri@gmail.com>
2025-11-24 21:47:17 +0530

    Time: 1 ms (100%), Space: 44 MB (17.83%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.java
+++ 3174-clear-digits/3174-clear-digits.java
@@ -16,33 +16,3 @@ class Solution {
         return sb.toString();
     }
 }
-//it takes 1ms
-
-
-
-
-/*     it takes 5ms to run
-
-
-class Solution {
-    public String clearDigits(String s) {
-        Stack<Character>st=new Stack<>();
-        String r="";
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(Character.isDigit(ch)){
-                if(!st.isEmpty()){
-                    st.pop();
-                }
-            }
-            else{
-                st.push(ch);
-            }
-        }
-       for(int i=0;i<st.size();i++){
-            char ch=st.get(i);
-            r=r+ch;
-                }
-        return r;
-    }
-}*/
