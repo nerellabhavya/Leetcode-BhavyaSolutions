87b447748c8876de0ec2434c936b877be7bbf7d1
nerellabhavya <nerellabhavyasri@gmail.com>
2025-11-24 22:06:06 +0530

    Time: 5 ms (38.95%), Space: 42.9 MB (44.89%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,5 +1,26 @@
 class Solution {
     public String removeOuterParentheses(String s) {
+        StringBuilder sb = new StringBuilder();
+        int depth = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+    char c = s.charAt(i);
+            if (c == '(') {
+                if (depth > 0) sb.append(c);
+                depth++;
+            } else {
+                depth--;
+                if (depth > 0) sb.append(c); 
+            }
+        }
+        
+        return sb.toString();
+    }
+}
+
+
+/*class Solution {
+    public String removeOuterParentheses(String s) {
         String r="";
         int oc=0;
         int cc=0;
@@ -21,4 +42,4 @@ class Solution {
         }
         return r;
     }
-}
+}*/
