e963b61cac9ca7846e92aa8b8f18aff7d7bd0c81
nerellabhavya <nerellabhavyasri@gmail.com>
2025-12-17 22:42:31 +0530

    Time: 2 ms (33.82%), Space: 42.7 MB (52.83%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -3,19 +3,16 @@ import java.util.PriorityQueue;
 class Solution {
     public int lastStoneWeight(int[] stones) {
 
-        // Max Heap
         PriorityQueue<Integer> pq =
                 new PriorityQueue<>((a, b) -> b - a);
 
-        // Insert all stones → O(n log n)
         for (int i = 0; i < stones.length; i++) {
             pq.add(stones[i]);
         }
 
-        // Each smash → log n
         while (pq.size() > 1) {
-            int y = pq.poll(); // largest
-            int x = pq.poll(); // second largest
+            int y = pq.poll(); 
+            int x = pq.poll(); 
 
             if (y != x) {
                 pq.add(y - x);
